ce7c61989b05a1fa17d91d2bc87349d34897e652
asonnens <asonnens@gmail.com>
2018-11-01 07:38:04 -0700

    improved handling for multiple variants

--- read_vcf.py
+++ read_vcf.py
@@ -9,6 +9,7 @@ def main():
     
 
 def read_VCF(filename):
+    #checks VCF version number, sends each variant to the function line_parse
     VCFfile = open(filename)
     header = VCFfile.readline()
     if "VCFv4.1" not in header:
@@ -24,33 +25,40 @@ def read_VCF(filename):
     return(list_of_variants)
 
 def line_parse(inline):
+    #pulls out relevant information from variant lines
     myline = inline.split("\t")
     chromosome = myline[0]
     position = myline[1]
     ref = myline[3]
     alt = myline[4]
-    if "," in alt:
+    if "," in alt:                 #checking if multiple variant alleles-- removing commas for future CSV file
         alt = alt.replace(",",":")
     info = myline[7]
     info_list = info.split(";")
     type = info_list[-1].split("=")[-1]
-    if "ins" in type:
-        type = "indel"
-    elif "del" in type:
-        type = "indel"
-    #if "complex" in info_list[-1]:
-    #    print ref, alt, info_list[-1]
+    if "," in type:
+        type = type.replace(",",":")      #classifying type of multiple variant alleles
+    if "ins" in type:             
+        type = type.replace("ins","insertion")
+    if "del" in type:
+        type = type.replace("del","deletion")
     coverage_info = myline[9]
     depth = coverage_info.split(":")[2]
     ref_count = float(coverage_info.split(":")[4])
     alt_count = coverage_info.split(":")[6]
-    if "," in alt_count:
+    ref_percent = "error"
+    alt_percent = "error"
+    if "," in alt_count:                 #if there's multiple alleles, reporting frequency for each separated by ":"
         alt_list = alt_count.split(",")
-        alt_count = sum([float(i) for i in alt_list])
+        alt_sum = sum([float(i) for i in alt_list])
+        ref_percent = str(100* ref_count/(ref_count + alt_sum))[0:4]
+        alt_percent = [str(100 * (float(i)/(ref_count + alt_sum)))[0:4] for i in alt_list] 
+        alt_percent = ":".join(alt_percent)
     else:
-        alt_count = float(coverage_info.split(":")[6])    
-    ref_percent = str(100* ref_count/(ref_count + alt_count))[0:4]
-    alt_percent = str(100* alt_count/(ref_count + alt_count))[0:4]
+        alt_count = float(coverage_info.split(":")[6])
+        alt_sum = alt_count  
+        alt_percent = str(100* alt_count/(ref_count + alt_sum))[0:4]  
+        ref_percent = str(100* ref_count/(ref_count + alt_sum))[0:4]
     return_line = [chromosome, position, ref, alt, type, depth,ref_percent,alt_percent]
     return(return_line)
 
